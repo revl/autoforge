5191a060ab4112fc4e69d307d2c12a6e5f263651
Damon Revoe <him@revl.org>
2017-03-24 22:41:29 -0400

    Specify a positional argument in the usage text

--- main.go
+++ main.go
@@ -2,8 +2,17 @@ package main
 
 import "flag"
 import "fmt"
+import "os"
+
+func autoforgeUsage() {
+	fmt.Printf("Usage: %s [options] [package_range]\n\n", os.Args[0])
+
+	flag.PrintDefaults()
+}
 
 func main() {
+	flag.Usage = autoforgeUsage
+
 	init := flag.Bool("init", false, "Initialize the working copy")
 
 	flag.Parse()
