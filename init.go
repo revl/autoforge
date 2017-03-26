021a2de7487efb3e9bd10a1c94c245f2cd15094a
Damon Revoe <him@revl.org>
2017-03-25 22:23:49 -0400

    Start -init implementation

---
+++ init.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"gopkg.in/yaml.v2"
+)
+
+type initParams struct {
+	OutputDir string `yaml:"outputdir,omitempty"`
+}
+
+func loadInitParams() initParams {
+	return initParams{}
+}
+
+func initializeWorkspace(installdir, docdir, maketarget string, quiet bool) {
+	fmt.Println("Initializing a new workspace...")
+	ip := initParams{OutputDir: "/home"}
+	out, err := yaml.Marshal(&ip)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(out))
+}
--- main.go
+++ main.go
@@ -1,23 +1,47 @@
 package main
 
-import "flag"
-import "fmt"
-import "os"
-
-func autoforgeUsage() {
-	fmt.Printf("Usage: %s [options] [package_range]\n\n", os.Args[0])
-
-	flag.PrintDefaults()
-}
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
 func main() {
-	flag.Usage = autoforgeUsage
+	// Handle panics by printing the error and exiting with return code 1.
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Fprintf(os.Stderr, "autoforge: %s\n", err)
 
-	init := flag.Bool("init", false, "Initialize the working copy")
+			os.Exit(1)
+		}
+	}()
+
+	// Parse and process command line arguments.
+
+	flag.Usage = func() {
+		fmt.Printf("Usage: %s [options] [package_range]\n\n",
+			os.Args[0])
+
+		flag.PrintDefaults()
+	}
+
+	initFlag := flag.Bool("init", false, "initialize a new workspace")
+
+	installdir := flag.String("installdir", "",
+		"target directory for 'make install'")
+
+	docdir := flag.String("docdir", "",
+		"installation directory for documentation")
+
+	maketarget := flag.String("maketarget", "help",
+		"default makefile target")
+
+	quiet := flag.Bool("quiet", false,
+		"do not log progress to standard output")
 
 	flag.Parse()
 
-	if *init {
-		fmt.Println("Initializing the working copy...")
+	if *initFlag {
+		initializeWorkspace(*installdir, *docdir, *maketarget, *quiet)
 	}
 }
