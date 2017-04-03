d79e5842dcd0782563c0a956d3a365222fbc94cc
Damon Revoe <him@revl.org>
2017-04-03 19:09:16 -0400

    Add greeting.yaml

--- query_cmd.go
+++ query_cmd.go
@@ -4,4 +4,8 @@ import "fmt"
 
 func queryPackages(workspacedir, pkgpath string) {
 	fmt.Println("List of packages:")
+
+	pd := loadPackageDefinition("examples/packages/greeting/greeting.yaml")
+
+	fmt.Println(pd.Name)
 }
