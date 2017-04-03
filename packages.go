1d7b625d6138e35abd94df34a7ca69c3aa6a71ce
Damon Revoe <him@revl.org>
2017-04-02 22:30:00 -0400

    Add a draft of packages.go

---
+++ packages.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"io/ioutil"
+	"gopkg.in/yaml.v2"
+)
+
+type packageDefinition struct {
+	Name string `yaml:"name"`
+}
+
+func loadPackageDefinition(pathname string) packageDefinition {
+	data, err := ioutil.ReadFile(pathname)
+
+	if err != nil {
+		panic(err)
+	}
+
+	var pd packageDefinition
+
+	err = yaml.Unmarshal(data, &pd)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return pd
+}
