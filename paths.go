16ed2a1220d53e3ef64a7f02c149a7024773a419
Damon Revoe <him@revl.org>
2017-06-02 22:37:40 -0400

    Advance pathname template implementation

--- paths.go
+++ paths.go
@@ -27,28 +27,41 @@ type verbatim struct {
 	next *array
 }
 
-func (self *verbatim) subst(paramName string, paramValue interface{}) {
+// Subst updates the 'verbatim' receiver by replacing all instances
+// of paramName surrounded by braces with paramValue, which can be
+// either a string or a slice of strings. In the latter case, the
+// text in the receiver structure gets truncated by the substitution
+// and the receiver structure gets extended by a new array structure.
+func (v *verbatim) subst(paramName string, paramValue interface{}) {
 	textValue, ok := paramValue.(string)
+
 	if ok {
-		self.text = strings.Replace(self.text, "{"+paramName+"}", textValue, -1)
+		v.text = strings.Replace(v.text, "{"+paramName+"}", textValue, -1)
 	} else {
 		arrayValue, ok := paramValue.([]string)
+
 		if ok {
-			pos := strings.Index(self.text, "{"+paramName+"}")
+			pos := strings.Index(v.text, "{"+paramName+"}")
+
 			if pos >= 0 {
-				self.next = &array{paramName, arrayValue,
-					verbatim{self.text[len(paramName)+2:], self.next}}
-				self.text = self.text[:pos]
+				v.next = &array{paramName, arrayValue,
+					verbatim{v.text[len(paramName)+2:], v.next}}
+				v.text = v.text[:pos]
 			}
 		} else {
-			self.text = strings.Replace(self.text, "{"+paramName+"}",
+			v.text = strings.Replace(v.text, "{"+paramName+"}",
 				fmt.Sprint(paramValue), -1)
 		}
 	}
 }
 
+// ExpandPathnameTemplate takes a pathname template and subsitutes
+// template parameter names with their values. Parameter values can be
+// either strings or slices of strings. Each template value that is a
+// slice of strings multiplies the number of output strings by the number
+// of strings in the slice.
 func expandPathnameTemplate(pathname string, params templateParams) []fileParams {
-	root := &verbatim{pathname, nil}
+	root := verbatim{pathname, nil}
 
 	for paramName, paramValue := range params {
 		root.subst(paramName, paramValue)
@@ -59,6 +72,10 @@ func expandPathnameTemplate(pathname string, params templateParams) []fileParams
 
 		fmt.Println(paramName, paramValue)
 		strings.Index(pathname, "{"+paramName+"}")
+	}
+
+	if root.next == nil {
+		return []fileParams{{root.text, params}}
 	}
 
 	fmt.Println(root)
