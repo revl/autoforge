cbb4ae17f41cda51d4096ae2f46f3956c9c51b20
Damon Revoe <him@revl.org>
2017-06-16 18:40:52 -0400

    Implement first version of expandPathnameTemplate

--- paths.go
+++ paths.go
@@ -32,7 +32,8 @@ type verbatim struct {
 // either a string or a slice of strings. In the latter case, the
 // text in the receiver structure gets truncated by the substitution
 // and the receiver structure gets extended by a new array structure.
-func (v *verbatim) subst(paramName string, paramValue interface{}) {
+// Subst returns the number of substitution values.
+func (v *verbatim) subst(paramName string, paramValue interface{}) int {
 	if textValue, ok := paramValue.(string); ok {
 		v.text = strings.Replace(v.text, "{"+paramName+"}",
 			textValue, -1)
@@ -44,7 +45,10 @@ func (v *verbatim) subst(paramName string, paramValue interface{}) {
 			verbatim{v.text[pos+len(paramName)+2:],
 				v.next}}
 		v.text = v.text[:pos]
+		return len(arrayValue)
 	}
+
+	return 1
 }
 
 // ExpandPathnameTemplate takes a pathname template and subsitutes
@@ -52,25 +56,56 @@ func (v *verbatim) subst(paramName string, paramValue interface{}) {
 // either strings or slices of strings. Each template value that is a
 // slice of strings multiplies the number of output strings by the number
 // of strings in the slice.
-func expandPathnameTemplate(pathname string, params templateParams) []fileParams {
+func expandPathnameTemplate(pathname string,
+	params templateParams) []fileParams {
 	root := verbatim{pathname, nil}
 
+	resultSize := 1
+
 	for paramName, paramValue := range params {
-		root.subst(paramName, paramValue)
+		resultSize *= root.subst(paramName, paramValue)
 
-		for node := root.next; node != nil; node = node.continuation.next {
-			node.continuation.subst(paramName, paramValue)
+		for n := root.next; n != nil; n = n.continuation.next {
+			resultSize *= n.continuation.subst(paramName, paramValue)
 		}
-
-		fmt.Println(paramName, paramValue)
-		strings.Index(pathname, "{"+paramName+"}")
 	}
 
-	if root.next == nil {
-		return []fileParams{{root.text, params}}
+	result := make([]fileParams, resultSize)
+
+	for i := 0; i < resultSize; i++ {
+		result[i].filename = root.text
+		copyOfParams := templateParams{}
+		for paramName, paramValue := range params {
+			copyOfParams[paramName] = paramValue
+		}
+		result[i].params = copyOfParams
 	}
 
-	fmt.Println(root)
+	if resultSize == 0 {
+		return result
+	}
 
-	return []fileParams{{pathname, params}}
+	sliceSize := resultSize
+
+	for a := root.next; a != nil; a = a.continuation.next {
+		numberOfSlices := resultSize / sliceSize
+		numberOfValues := len(a.paramValues)
+		sliceSize /= numberOfValues
+
+		filenameIndex := 0
+		verbatimText := a.continuation.text
+		for i := 0; i < numberOfSlices; i++ {
+			for j := 0; j < numberOfValues; j++ {
+				paramValue := a.paramValues[j]
+				filenameFragment := paramValue + verbatimText
+				for k := 0; k < sliceSize; k++ {
+					result[filenameIndex].filename += filenameFragment
+					result[filenameIndex].params[a.paramName] = paramValue
+					filenameIndex++
+				}
+			}
+		}
+	}
+
+	return result
 }
