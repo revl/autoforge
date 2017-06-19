3a50d1686cc46e659a234665ac978a5a8012cf04
Damon Revoe <him@revl.org>
2017-06-19 16:32:55 -0400

    Add checking to the test of expandPathnameTemplate

--- paths_test.go
+++ paths_test.go
@@ -5,7 +5,7 @@
 package main
 
 import (
-	"fmt"
+	"reflect"
 	"strings"
 	"testing"
 )
@@ -43,11 +43,71 @@ func TestSubst(t *testing.T) {
 	substTestCase(t, &v, "Mirror, Mirror on the Wall")
 }
 
+func runExpandPathnameTemplateTest(t *testing.T,
+	pathname string, params map[string]interface{}, expected []fileParams) {
+
+	result := expandPathnameTemplate(pathname, params)
+	if !reflect.DeepEqual(result, expected) {
+		t.Error("Result", result,
+			"and expected result", expected, "are not equal")
+	}
+}
+
 func TestExpandPathnameTemplate(t *testing.T) {
-	fmt.Println(expandPathnameTemplate("{nil}{dir}/{name}.{ext}",
-		map[string]interface{}{
-			"nil":  []string{},
-			"dir":  []string{"red", "blue", "yellow", "green"},
-			"name": []string{"foo", "bar"},
-			"ext":  []string{"js", "go", "rs"}}))
+	params2x3 := map[string]interface{}{
+		"name": []string{"foo", "bar"},
+		"ext":  []string{"js", "go", "rs"}}
+
+	result2x3 := []fileParams{
+		{"foo.js", templateParams{"name": "foo", "ext": "js"}},
+		{"bar.js", templateParams{"name": "bar", "ext": "js"}},
+		{"foo.go", templateParams{"name": "foo", "ext": "go"}},
+		{"bar.go", templateParams{"name": "bar", "ext": "go"}},
+		{"foo.rs", templateParams{"name": "foo", "ext": "rs"}},
+		{"bar.rs", templateParams{"name": "bar", "ext": "rs"}}}
+
+	runExpandPathnameTemplateTest(t, "{name}.{ext}", params2x3, result2x3)
+
+	params4x2x3 := map[string]interface{}{
+		"dir":  []string{"A", "B", "C", "D"},
+		"name": []string{"1", "2"},
+		"ext":  []string{"a", "b", "c"}}
+
+	result4x2x3 := []fileParams{
+		{"A/1.a", templateParams{"dir": "A", "name": "1", "ext": "a"}},
+		{"B/1.a", templateParams{"dir": "B", "name": "1", "ext": "a"}},
+		{"C/1.a", templateParams{"dir": "C", "name": "1", "ext": "a"}},
+		{"D/1.a", templateParams{"dir": "D", "name": "1", "ext": "a"}},
+		{"A/2.a", templateParams{"dir": "A", "name": "2", "ext": "a"}},
+		{"B/2.a", templateParams{"dir": "B", "name": "2", "ext": "a"}},
+		{"C/2.a", templateParams{"dir": "C", "name": "2", "ext": "a"}},
+		{"D/2.a", templateParams{"dir": "D", "name": "2", "ext": "a"}},
+		{"A/1.b", templateParams{"dir": "A", "name": "1", "ext": "b"}},
+		{"B/1.b", templateParams{"dir": "B", "name": "1", "ext": "b"}},
+		{"C/1.b", templateParams{"dir": "C", "name": "1", "ext": "b"}},
+		{"D/1.b", templateParams{"dir": "D", "name": "1", "ext": "b"}},
+		{"A/2.b", templateParams{"dir": "A", "name": "2", "ext": "b"}},
+		{"B/2.b", templateParams{"dir": "B", "name": "2", "ext": "b"}},
+		{"C/2.b", templateParams{"dir": "C", "name": "2", "ext": "b"}},
+		{"D/2.b", templateParams{"dir": "D", "name": "2", "ext": "b"}},
+		{"A/1.c", templateParams{"dir": "A", "name": "1", "ext": "c"}},
+		{"B/1.c", templateParams{"dir": "B", "name": "1", "ext": "c"}},
+		{"C/1.c", templateParams{"dir": "C", "name": "1", "ext": "c"}},
+		{"D/1.c", templateParams{"dir": "D", "name": "1", "ext": "c"}},
+		{"A/2.c", templateParams{"dir": "A", "name": "2", "ext": "c"}},
+		{"B/2.c", templateParams{"dir": "B", "name": "2", "ext": "c"}},
+		{"C/2.c", templateParams{"dir": "C", "name": "2", "ext": "c"}},
+		{"D/2.c", templateParams{"dir": "D", "name": "2", "ext": "c"}}}
+
+	runExpandPathnameTemplateTest(t, "{dir}/{name}.{ext}",
+		params4x2x3, result4x2x3)
+
+	paramsNil := map[string]interface{}{
+		"nil":      []string{},
+		"noeffect": []string{"value"}}
+
+	resultNil := []fileParams{}
+
+	runExpandPathnameTemplateTest(t, "{nil}/{noeffect}",
+		paramsNil, resultNil)
 }
