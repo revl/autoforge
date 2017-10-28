07053e053b7d4a784f47b8390d13cafa30b91cf1
Damon Revoe <him@revl.org>
2017-10-27 22:56:03 -0400

    Extract path templating into dedicated files

---
+++ pathtmpl.go
@@ -0,0 +1,108 @@
+// Copyright (C) 2017 Damon Revoe. All rights reserved.
+// Use of this source code is governed by the MIT
+// license, which can be found in the LICENSE file.
+
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+type outputFileParams struct {
+	filename string
+	params   templateParams
+}
+
+type pathnameTemplateMultiplier struct {
+	paramName    string
+	paramValues  []string
+	continuation pathnameTemplateText
+}
+
+type pathnameTemplateText struct {
+	text string
+	next *pathnameTemplateMultiplier
+}
+
+// Subst updates the 'pathnameTemplateText' receiver by replacing all
+// instances of 'name' surrounded by braces with 'value', which can be
+// either a string or a slice of strings.  In the latter case, the text
+// in the receiver structure gets truncated by the substitution and the
+// receiver structure gets extended by a new pathnameTemplateMultiplier
+// structure.  Subst returns the number of substitution values.
+func (t *pathnameTemplateText) subst(name string, value interface{}) int {
+	if textValue, ok := value.(string); ok {
+		t.text = strings.Replace(t.text, "{"+name+"}",
+			textValue, -1)
+	} else if arrayValue, ok := value.([]string); !ok {
+		t.text = strings.Replace(t.text, "{"+name+"}",
+			fmt.Sprint(value), -1)
+	} else if pos := strings.Index(t.text, "{"+name+"}"); pos >= 0 {
+		t.next = &pathnameTemplateMultiplier{name, arrayValue,
+			pathnameTemplateText{t.text[pos+len(name)+2:], t.next}}
+		t.text = t.text[:pos]
+		return len(arrayValue)
+	}
+
+	return 1
+}
+
+// ExpandPathnameTemplate takes a pathname template and substitutes
+// template parameter names with their values. Parameter values can be
+// either strings or slices of strings. Each template value that is a
+// slice of strings multiplies the number of output strings by the number
+// of strings in the slice.
+func expandPathnameTemplate(pathname string,
+	params templateParams) []outputFileParams {
+	root := pathnameTemplateText{pathname, nil}
+
+	resultSize := 1
+
+	for name, value := range params {
+		resultSize *= root.subst(name, value)
+
+		for n := root.next; n != nil; n = n.continuation.next {
+			resultSize *= n.continuation.subst(name, value)
+		}
+	}
+
+	result := make([]outputFileParams, resultSize)
+
+	for i := 0; i < resultSize; i++ {
+		result[i].filename = root.text
+		copyOfParams := templateParams{}
+		for name, value := range params {
+			copyOfParams[name] = value
+		}
+		result[i].params = copyOfParams
+	}
+
+	if resultSize == 0 {
+		return result
+	}
+
+	sliceSize := 1
+
+	for a := root.next; a != nil; a = a.continuation.next {
+		numberOfValues := len(a.paramValues)
+
+		continuationText := a.continuation.text
+
+		for i := 0; i < resultSize; {
+			for j := 0; j < numberOfValues; j++ {
+				value := a.paramValues[j]
+				filenameFragment := value + continuationText
+				for k := 0; k < sliceSize; k++ {
+					result[i].filename += filenameFragment
+					result[i].params[a.paramName] = value
+					i++
+				}
+			}
+		}
+
+		sliceSize *= numberOfValues
+	}
+
+	return result
+}
---
+++ pathtmpl_test.go
@@ -0,0 +1,113 @@
+// Copyright (C) 2017 Damon Revoe. All rights reserved.
+// Use of this source code is governed by the MIT
+// license, which can be found in the LICENSE file.
+
+package main
+
+import (
+	"reflect"
+	"strings"
+	"testing"
+)
+
+func substTestCase(t *testing.T, node *pathnameTemplateText, expected string) {
+	var result string
+
+	for {
+		result += node.text
+		if node.next == nil {
+			break
+		}
+
+		result += "[" + strings.Join(node.next.paramValues, ", ") + "]"
+
+		node = &node.next.continuation
+	}
+
+	if result != expected {
+		t.Error("Error: \"" + result + "\" != \"" + expected + "\"")
+	}
+}
+
+func TestSubst(t *testing.T) {
+	v := pathnameTemplateText{"{Greetings}, {Who}!", nil}
+	v.subst("Who", "Human")
+	v.subst("Greetings", []string{"Hello", "Hi"})
+
+	substTestCase(t, &v, "[Hello, Hi], Human!")
+
+	v = pathnameTemplateText{"{What}, {What} {Where}", nil}
+	v.subst("What", "Mirror")
+	v.subst("Where", "on the Wall")
+
+	substTestCase(t, &v, "Mirror, Mirror on the Wall")
+}
+
+func runExpandPathnameTemplateTest(t *testing.T,
+	pathname string, params map[string]interface{}, expected []outputFileParams) {
+
+	result := expandPathnameTemplate(pathname, params)
+	if !reflect.DeepEqual(result, expected) {
+		t.Error("Result", result,
+			"and expected result", expected, "are not equal")
+	}
+}
+
+func TestExpandPathnameTemplate(t *testing.T) {
+	params2x3 := map[string]interface{}{
+		"name": []string{"foo", "bar"},
+		"ext":  []string{"js", "go", "rs"}}
+
+	result2x3 := []outputFileParams{
+		{"foo.js", templateParams{"name": "foo", "ext": "js"}},
+		{"bar.js", templateParams{"name": "bar", "ext": "js"}},
+		{"foo.go", templateParams{"name": "foo", "ext": "go"}},
+		{"bar.go", templateParams{"name": "bar", "ext": "go"}},
+		{"foo.rs", templateParams{"name": "foo", "ext": "rs"}},
+		{"bar.rs", templateParams{"name": "bar", "ext": "rs"}}}
+
+	runExpandPathnameTemplateTest(t, "{name}.{ext}", params2x3, result2x3)
+
+	params4x2x3 := map[string]interface{}{
+		"dir":  []string{"A", "B", "C", "D"},
+		"name": []string{"1", "2"},
+		"ext":  []string{"a", "b", "c"}}
+
+	result4x2x3 := []outputFileParams{
+		{"A/1.a", templateParams{"dir": "A", "name": "1", "ext": "a"}},
+		{"B/1.a", templateParams{"dir": "B", "name": "1", "ext": "a"}},
+		{"C/1.a", templateParams{"dir": "C", "name": "1", "ext": "a"}},
+		{"D/1.a", templateParams{"dir": "D", "name": "1", "ext": "a"}},
+		{"A/2.a", templateParams{"dir": "A", "name": "2", "ext": "a"}},
+		{"B/2.a", templateParams{"dir": "B", "name": "2", "ext": "a"}},
+		{"C/2.a", templateParams{"dir": "C", "name": "2", "ext": "a"}},
+		{"D/2.a", templateParams{"dir": "D", "name": "2", "ext": "a"}},
+		{"A/1.b", templateParams{"dir": "A", "name": "1", "ext": "b"}},
+		{"B/1.b", templateParams{"dir": "B", "name": "1", "ext": "b"}},
+		{"C/1.b", templateParams{"dir": "C", "name": "1", "ext": "b"}},
+		{"D/1.b", templateParams{"dir": "D", "name": "1", "ext": "b"}},
+		{"A/2.b", templateParams{"dir": "A", "name": "2", "ext": "b"}},
+		{"B/2.b", templateParams{"dir": "B", "name": "2", "ext": "b"}},
+		{"C/2.b", templateParams{"dir": "C", "name": "2", "ext": "b"}},
+		{"D/2.b", templateParams{"dir": "D", "name": "2", "ext": "b"}},
+		{"A/1.c", templateParams{"dir": "A", "name": "1", "ext": "c"}},
+		{"B/1.c", templateParams{"dir": "B", "name": "1", "ext": "c"}},
+		{"C/1.c", templateParams{"dir": "C", "name": "1", "ext": "c"}},
+		{"D/1.c", templateParams{"dir": "D", "name": "1", "ext": "c"}},
+		{"A/2.c", templateParams{"dir": "A", "name": "2", "ext": "c"}},
+		{"B/2.c", templateParams{"dir": "B", "name": "2", "ext": "c"}},
+		{"C/2.c", templateParams{"dir": "C", "name": "2", "ext": "c"}},
+		{"D/2.c", templateParams{"dir": "D", "name": "2", "ext": "c"}}}
+
+	runExpandPathnameTemplateTest(t, "{dir}/{name}.{ext}",
+		params4x2x3, result4x2x3)
+
+	paramsNil := map[string]interface{}{
+		"nil":      []string{},
+		"noeffect": []string{"value"}}
+
+	resultNil := []outputFileParams{}
+
+	runExpandPathnameTemplateTest(t, "{nil}/{noeffect}",
+		paramsNil, resultNil)
+}
