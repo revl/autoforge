83b847e98145b0e7def5a5b5cc081717aadb3b5a
Damon Revoe <him@revl.org>
2017-10-30 23:37:57 -0400

    Add the refresh command

---
+++ refreshcmd.go
@@ -0,0 +1,30 @@
+// Copyright (C) 2017 Damon Revoe. All rights reserved.
+// Use of this source code is governed by the MIT
+// license, which can be found in the LICENSE file.
+
+package main
+
+import (
+	"log"
+
+	"github.com/spf13/cobra"
+)
+
+// RefreshCmd represents the refresh command
+var refreshCmd = &cobra.Command{
+	Use:   "refresh",
+	Short: "Regenerate Autotools files in the current workspace",
+	Run: func(_ *cobra.Command, args []string) {
+		if err := generatePackageSources([]string{}); err != nil {
+			log.Fatal(err)
+		}
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(refreshCmd)
+
+	refreshCmd.Flags().SortFlags = false
+	addQuietFlag(refreshCmd)
+	addWorkspaceDirFlag(refreshCmd)
+}
