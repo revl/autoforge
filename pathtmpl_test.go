d90ca7af27449eee235c99aecc9693f3a3f03de6
Damon Revoe <him@revl.org>
2017-12-08 21:38:40 -0500

    Update pathtmpl_test

--- pathtmpl_test.go
+++ pathtmpl_test.go
@@ -44,7 +44,8 @@ func TestSubst(t *testing.T) {
 }
 
 func runExpandPathnameTemplateTest(t *testing.T,
-	pathname string, params map[string]interface{}, expected []outputFileParams) {
+	pathname string, params map[string]interface{},
+	expected []outputFileParams) {
 
 	result := expandPathnameTemplate(pathname, params)
 	if !reflect.DeepEqual(result, expected) {
@@ -53,55 +54,60 @@ func runExpandPathnameTemplateTest(t *testing.T,
 	}
 }
 
-func TestExpandPathnameTemplate(t *testing.T) {
+func TestExpandPathnameTemplate2x3(t *testing.T) {
 	params2x3 := map[string]interface{}{
 		"name": []string{"foo", "bar"},
 		"ext":  []string{"js", "go", "rs"}}
 
-	result2x3 := []outputFileParams{
-		{"foo.js", templateParams{"name": "foo", "ext": "js"}},
-		{"bar.js", templateParams{"name": "bar", "ext": "js"}},
-		{"foo.go", templateParams{"name": "foo", "ext": "go"}},
-		{"bar.go", templateParams{"name": "bar", "ext": "go"}},
-		{"foo.rs", templateParams{"name": "foo", "ext": "rs"}},
-		{"bar.rs", templateParams{"name": "bar", "ext": "rs"}}}
+	var result2x3 []outputFileParams
+
+	for _, ext := range params2x3["ext"].([]string) {
+		for _, name := range params2x3["name"].([]string) {
+			filename := name + "." + ext
+
+			result2x3 = append(result2x3,
+				outputFileParams{filename,
+					templateParams{
+						"name":     name,
+						"ext":      ext,
+						"filename": filename,
+						"dirname":  "."}})
+		}
+	}
 
 	runExpandPathnameTemplateTest(t, "{name}.{ext}", params2x3, result2x3)
+}
 
+func TestExpandPathnameTemplate4x2x3(t *testing.T) {
 	params4x2x3 := map[string]interface{}{
 		"dir":  []string{"A", "B", "C", "D"},
 		"name": []string{"1", "2"},
 		"ext":  []string{"a", "b", "c"}}
 
-	result4x2x3 := []outputFileParams{
-		{"A/1.a", templateParams{"dir": "A", "name": "1", "ext": "a"}},
-		{"B/1.a", templateParams{"dir": "B", "name": "1", "ext": "a"}},
-		{"C/1.a", templateParams{"dir": "C", "name": "1", "ext": "a"}},
-		{"D/1.a", templateParams{"dir": "D", "name": "1", "ext": "a"}},
-		{"A/2.a", templateParams{"dir": "A", "name": "2", "ext": "a"}},
-		{"B/2.a", templateParams{"dir": "B", "name": "2", "ext": "a"}},
-		{"C/2.a", templateParams{"dir": "C", "name": "2", "ext": "a"}},
-		{"D/2.a", templateParams{"dir": "D", "name": "2", "ext": "a"}},
-		{"A/1.b", templateParams{"dir": "A", "name": "1", "ext": "b"}},
-		{"B/1.b", templateParams{"dir": "B", "name": "1", "ext": "b"}},
-		{"C/1.b", templateParams{"dir": "C", "name": "1", "ext": "b"}},
-		{"D/1.b", templateParams{"dir": "D", "name": "1", "ext": "b"}},
-		{"A/2.b", templateParams{"dir": "A", "name": "2", "ext": "b"}},
-		{"B/2.b", templateParams{"dir": "B", "name": "2", "ext": "b"}},
-		{"C/2.b", templateParams{"dir": "C", "name": "2", "ext": "b"}},
-		{"D/2.b", templateParams{"dir": "D", "name": "2", "ext": "b"}},
-		{"A/1.c", templateParams{"dir": "A", "name": "1", "ext": "c"}},
-		{"B/1.c", templateParams{"dir": "B", "name": "1", "ext": "c"}},
-		{"C/1.c", templateParams{"dir": "C", "name": "1", "ext": "c"}},
-		{"D/1.c", templateParams{"dir": "D", "name": "1", "ext": "c"}},
-		{"A/2.c", templateParams{"dir": "A", "name": "2", "ext": "c"}},
-		{"B/2.c", templateParams{"dir": "B", "name": "2", "ext": "c"}},
-		{"C/2.c", templateParams{"dir": "C", "name": "2", "ext": "c"}},
-		{"D/2.c", templateParams{"dir": "D", "name": "2", "ext": "c"}}}
+	var result4x2x3 []outputFileParams
+
+	for _, ext := range params4x2x3["ext"].([]string) {
+		for _, name := range params4x2x3["name"].([]string) {
+			for _, dir := range params4x2x3["dir"].([]string) {
+				filename := dir + "/" + name + "." + ext
+
+				result4x2x3 = append(result4x2x3,
+					outputFileParams{filename,
+						templateParams{
+							"dir":      dir,
+							"name":     name,
+							"ext":      ext,
+							"filename": filename,
+							"dirname":  dir}})
+			}
+		}
+	}
 
 	runExpandPathnameTemplateTest(t, "{dir}/{name}.{ext}",
 		params4x2x3, result4x2x3)
+}
 
+func TestExpandPathnameTemplateNoFiles(t *testing.T) {
 	paramsNil := map[string]interface{}{
 		"nil":      []string{},
 		"noeffect": []string{"value"}}
